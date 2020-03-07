3eb5c630f2cd12fbe1cd0d7c5c947462c2a9aa75
Duc T <ductran963@gmail.com>
2020-03-07 15:09:40 -0800

    working models, added comments

--- models/book.js
+++ models/book.js
@@ -1,6 +1,7 @@
+// adding database using mongoose
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
+// setting up new schema for database and setting restrictions/validations
 const bookSchema = new Schema({
   googleId: { type: String, required: true },
   title: { type: String, required: true },
@@ -10,7 +11,7 @@ const bookSchema = new Schema({
   link: String,
   date: { type: Date, default: Date.now }
 });
-
+//including id for each so we can use that and delete books according to their id
 bookSchema.index({
   googleId: 1,
 }, {
